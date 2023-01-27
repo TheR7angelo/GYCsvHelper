1bcc00f0b2bbe756e739661bb4b9bb6d61c61df1
TheR7angelo <raphael.antunes@hotmail.fr>
2023-01-27 15:13:21 +0100

    update

--- GYCsvHelper/Libs/CsvReader/Common.cs
+++ GYCsvHelper/Libs/CsvReader/Common.cs
@@ -5,10 +5,17 @@ using CsvHelper.Configuration;
 namespace Libs.CsvReader;
 
 public static class Common
-{ 
-    public static CsvConfiguration GetConfigurationReader => new (CultureInfo.InvariantCulture)
-    { Delimiter = ";", Encoding = Encoding.Latin1, BadDataFound = null };
-    
-    public static CsvConfiguration GetConfigurationWriter => new (CultureInfo.InvariantCulture)
-        { Delimiter = ";", Encoding = Encoding.Latin1, BadDataFound = null, HasHeaderRecord = false};
+{
+    public static CsvConfiguration GetConfigurationReader => new(CultureInfo.InvariantCulture)
+        { Delimiter = ";", Encoding = Encoding.Latin1, BadDataFound = null };
+
+    public static CsvConfiguration GetConfigurationWriter => new(CultureInfo.InvariantCulture)
+        { Delimiter = ";", Encoding = Encoding.Latin1, BadDataFound = null, HasHeaderRecord = false };
+
+    public static string GetSavePath(this SStruc.SFilePath filePath, string baseName)
+    {
+        var date = DateTime.Now;
+
+        return Path.Join(filePath.FilePath, $"{baseName}{date:yyyy-MM-dd}{filePath.FileExtension}");
+    }
 }
