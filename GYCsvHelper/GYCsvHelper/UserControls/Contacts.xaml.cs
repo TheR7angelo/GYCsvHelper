4e44633b86f82b25bbc03b1c018eec388df51028
TheR7angelo <raphael.antunes@hotmail.fr>
2023-02-24 15:28:04 +0100

    add contact view

---
+++ GYCsvHelper/GYCsvHelper/UserControls/Contacts.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Data;
+using LibsPlaningAChaud.Sql;
+using LibsPlaningAChaud.Sql.Struc;
+
+namespace GYCsvHelper.UserControls;
+
+public partial class Contacts
+{
+    private SqlHandler _sqlHandler;
+    private Contact _contactSelected;
+
+    private CollectionView _collectionContacts;
+    public Contacts(SqlHandler sqlHandler)
+    {
+        InitializeComponent();
+        _sqlHandler = sqlHandler;
+        _contactSelected = new Contact();
+
+        var contact = sqlHandler.GetAllContact();
+        _collectionContacts = new CollectionView(contact);
+    }
+}
