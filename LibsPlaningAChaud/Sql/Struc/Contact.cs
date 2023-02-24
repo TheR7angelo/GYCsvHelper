8a1a7b1365b71cbe50592befabf3597a150547b3
TheR7angelo <raphael.antunes@hotmail.fr>
2023-02-24 15:28:55 +0100

    add contact class

---
+++ LibsPlaningAChaud/Sql/Struc/Contact.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LibsPlaningAChaud.Sql.Struc;
+
+public class Contact : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private int _id;
+
+    public int Id
+    {
+        get => _id;
+        set
+        {
+            _id = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _name = string.Empty;
+    
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _number = string.Empty;
+
+    public string Number
+    {
+        get => _number;
+        set
+        {
+            _number = value;
+            OnPropertyChanged();
+        }
+    }
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
