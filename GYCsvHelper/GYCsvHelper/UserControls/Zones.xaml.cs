714810f6f4d375e987a79796be798789cec8e13d
TheR7angelo <raphael.antunes@hotmail.fr>
2023-02-27 14:42:25 +0100

    update

--- GYCsvHelper/GYCsvHelper/UserControls/Zones.xaml.cs
+++ GYCsvHelper/GYCsvHelper/UserControls/Zones.xaml.cs
@@ -1,11 +1,25 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.ObjectModel;
+using LibsPlaningAChaud.Sql;
+using LibsPlaningAChaud.Sql.Struc;
 
 namespace GYCsvHelper.UserControls;
 
-public partial class Zones : UserControl
+public partial class Zones
 {
-    public Zones()
+    private readonly SqlHandler _sqlHandler;
+    public ObservableCollection<Zone> CollectionZones { get; } = new();
+    
+    public Zones(SqlHandler sqlHandler)
     {
+        _sqlHandler = sqlHandler;
         InitializeComponent();
+    }
+
+    private void GetAllZones()
+    {
+        var zones = _sqlHandler.GetAllZones();
+        
+        CollectionZones.Clear();
+        foreach (var zone in zones) CollectionZones.Add(zone);
     }
 }
