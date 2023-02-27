219a39330190fc6d51c74af054446c951025d2f0
TheR7angelo <raphael.antunes@hotmail.fr>
2023-02-27 14:03:57 +0100

    add function create / delete contact

--- GYCsvHelper/GYCsvHelper/UserControls/Contacts.xaml.cs
+++ GYCsvHelper/GYCsvHelper/UserControls/Contacts.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
@@ -12,7 +13,7 @@ namespace GYCsvHelper.UserControls;
 public partial class Contacts
 {
     private readonly SqlHandler _sqlHandler;
-    private readonly List<FrameworkElement> ListContactDefinition;
+    private readonly List<FrameworkElement> _listContactDefinition;
     private bool _newContact;
 
     public Contact ContactSelected { get; } = new();
@@ -25,10 +26,16 @@ public partial class Contacts
         _newContact = false;
 
         InitializeComponent();
-        ListContactDefinition = new List<FrameworkElement>(GridContactDefinition.Children.Cast<FrameworkElement>()
+        _listContactDefinition = new List<FrameworkElement>(GridContactDefinition.Children.Cast<FrameworkElement>()
             .Where(s => s.GetType() != typeof(Label)));
 
-        var contacts = sqlHandler.GetAllContact();
+        GetAllContacts();
+    }
+
+    private void GetAllContacts()
+    {
+        var contacts = _sqlHandler.GetAllContact();
+        CollectionContacts.Clear();
         foreach (var contact in contacts) CollectionContacts.Add(contact);
     }
 
@@ -39,6 +46,7 @@ public partial class Contacts
         Contact contact;
         if (button.IsChecked ?? false)
         {
+            _newContact = false;
             contact = ((sender as FrameworkElement)!.DataContext as Contact)!;
             SetEnableGrid(true);
         }
@@ -48,6 +56,22 @@ public partial class Contacts
             SetEnableGrid(false);
         }
 
+        UpdateGridContactDefinition(contact);
+    }
+
+    private void ButtonNewContact_OnClick(object sender, RoutedEventArgs e)
+    {
+        var button = ListBoxContacts.FindVisualChildren<ToggleButton>().FirstOrDefault(s => s.IsChecked.Equals(true));
+        if (button is not null) button.IsChecked = false;
+
+        var contact = new Contact();
+        _newContact = true;
+        UpdateGridContactDefinition(contact);
+        SetEnableGrid(true);
+    }
+
+    private void UpdateGridContactDefinition(Contact contact)
+    {
         foreach (var property in typeof(Contact).GetProperties().Where(p => p.CanWrite))
         {
             property.SetValue(ContactSelected, property.GetValue(contact, null), null);
@@ -56,9 +80,56 @@ public partial class Contacts
 
     private void SetEnableGrid(bool enable)
     {
-        foreach (var item in ListContactDefinition) item.IsEnabled = enable;
+        foreach (var item in _listContactDefinition) item.IsEnabled = enable;
     }
 
     private void ButtonValidChangeContact_OnClick(object sender, RoutedEventArgs e)
-        => _sqlHandler.UpdateCantact(ContactSelected);
+    {
+        if (_newContact)
+        {
+            var id = unchecked((int)_sqlHandler.NewContact(ContactSelected));
+            GetAllContacts();
+            UpdateLayout();
+
+            var p = ListBoxContacts.Items.Count;
+            var q = CollectionContacts.Count;
+            var buttons = ListBoxContacts.FindVisualChildren<ToggleButton>();
+            
+            // todo trouver un moyen de sélection le nouveau bouton
+            foreach (var button in buttons)
+            {
+                var contact = (Contact)button.DataContext;
+                
+                if (!contact.Id.Equals(id)) continue;
+
+                button.IsChecked = true;
+                break;
+            }
+            
+        }
+        else
+        {
+            _sqlHandler.UpdateCantact(ContactSelected);
+        }
+    }
+
+    private void ButtonDeleteContact_OnClick(object sender, RoutedEventArgs e)
+    {
+        var button = ListBoxContacts.FindVisualChildren<ToggleButton>().FirstOrDefault(s => s.IsChecked.Equals(true));
+        
+        string msg;
+        if (button is null)
+        {
+            msg = "Aucun contact n'a étais sélectionner";
+            
+        }
+        else
+        {
+            var contact = (Contact)button.DataContext;
+            _sqlHandler.DeleteContact(contact);
+            msg = $"Le contact {contact.LastName} {contact.FirstName} a étais supprimer";
+            GetAllContacts();
+        }
+        MessageBox.Show(msg);
+    }
 }
