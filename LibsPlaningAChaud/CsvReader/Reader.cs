ad2b4ca988389438efae40ca73c397a103526fe6
TheR7angelo <raphael.antunes@hotmail.fr>
2023-03-01 16:41:18 +0100

    add read only file

--- LibsPlaningAChaud/CsvReader/Reader.cs
+++ LibsPlaningAChaud/CsvReader/Reader.cs
@@ -6,7 +6,8 @@ public static class Reader
 {
     public static IEnumerable<ExportInterventionCsv> Read(string filePath, EActivity activity)
     {
-        using var reader = new StreamReader(filePath, Encoding.Latin1);
+        using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        using var reader = new StreamReader(fileStream, Encoding.Latin1);
         var csv = new CsvHelper.CsvReader(reader, Common.GetConfigurationReader);
 
         var records = csv.GetRecords<ExportInterventionCsv>().ToList();
